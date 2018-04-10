14726dd523633b1ddde9477b3a7c0aba9ce0d67c
CU-Joel <32112929+CU-Joel@users.noreply.github.com>
2018-04-10 16:03:40 -0600

    EqualParity()'s completed and main.cpp setup for testing the functions

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  PE14
+//
+//  Created by Joel Davidson on 4/10/18.
+//  Copyright © 2018 Joel Davidson. All rights reserved.
+//
+
+#include <iostream>
+#include "math_utils.h"
+#include <vector>
+using namespace std;
+
+int main() {
+    int x = 1;
+    int y = -1;
+    vector<int> nums;
+    nums.push_back(x);
+    nums.push_back(y);
+    int p = EqualParity(x, y);
+    cout << "\nEqual parity returned " << p;
+    int q = EqualParity(nums);
+    cout << "\nEqual parity returned " << q << endl;
+    
+    return 0;
+}
--- math_utils.h
+++ math_utils.h
@@ -1,17 +1,24 @@
-//#ifndef math_utils_h
-//#define math_utils_h
+//
+//  math_utils.hpp
+//  PE14
+//
+//  Created by Joel Davidson on 4/10/18.
+//  Copyright © 2018 Joel Davidson. All rights reserved.
+//
+
+#ifndef math_utils_hpp
+#define math_utils_hpp
 
 #include <stdio.h>
 #include <string>
 #include <cstring>
 #include <iostream>
-
+#include <vector>
 
 
 bool IsSquare(int num);
 int GetDigit(int num, int place);
 bool EqualParity(int x, int y);
 bool EqualParity(std::vector<int> nums);
-    
-    
 
+#endif /* math_utils_hpp */
